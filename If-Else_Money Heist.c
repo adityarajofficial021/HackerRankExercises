7c63daceb6847176edbcb1e76229e5ab67a0ff74
Aditya Raj <adityarajofficial021@gmail.com>
2025-12-31 22:45:36 +0530

    Add files via upload

---
+++ If-Else_Money Heist.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+
+int main() {
+    
+    int d0, d1, d2, d3, d4, d5, d6, d7, d8, d9;
+    int position, operation;
+    scanf("%d %d %d %d %d %d %d %d %d %d", &d0, &d1, &d2, &d3, &d4, &d5, &d6, &d7, &d8, &d9);
+    scanf("%d %d", &position, &operation);
+    
+    if(position == 0){d0 =(d0 + operation) % 10;}
+     else if(position == 1){d1 =(d1 + operation) % 10;}
+     else if(position == 2){d2 =(d2 + operation) % 10;}
+     else if(position == 3){d3 =(d3 + operation) % 10;}
+     else if(position == 4){d4 =(d4 + operation) % 10;}
+     else if(position == 5){d5 =(d5 + operation) % 10;}
+     else if(position == 6){d6 =(d6 + operation) % 10;}
+     else if(position == 7){d7 =(d7 + operation) % 10;}
+     else if(position == 8){d8 =(d8 + operation) % 10;}
+     else if(position == 9){d9 =(d9 + operation) % 10;}
+    
+     scanf("%d %d", &position, &operation);
+    
+    if(position == 0){d0 =(d0 + operation) % 10;}
+     else if(position == 1){d1 =(d1 + operation) % 10;}
+     else if(position == 2){d2 =(d2 + operation) % 10;}
+     else if(position == 3){d3 =(d3 + operation) % 10;}
+     else if(position == 4){d4 =(d4 + operation) % 10;}
+     else if(position == 5){d5 =(d5 + operation) % 10;}
+     else if(position == 6){d6 =(d6 + operation) % 10;}
+     else if(position == 7){d7 =(d7 + operation) % 10;}
+     else if(position == 8){d8 =(d8 + operation) % 10;}
+     else if(position == 9){d9 =(d9 + operation) % 10;}
+    
+    
+     scanf("%d %d", &position, &operation);
+    
+    if(position == 0){d0 =(d0 + operation) % 10;}
+     else if(position == 1){d1 =(d1 + operation) % 10;}
+     else if(position == 2){d2 =(d2 + operation) % 10;}
+     else if(position == 3){d3 =(d3 + operation) % 10;}
+     else if(position == 4){d4 =(d4 + operation) % 10;}
+     else if(position == 5){d5 =(d5 + operation) % 10;}
+     else if(position == 6){d6 =(d6 + operation) % 10;}
+     else if(position == 7){d7 =(d7 + operation) % 10;}
+     else if(position == 8){d8 =(d8 + operation) % 10;}
+     else if(position == 9){d9 =(d9 + operation) % 10;}
+    
+    
+    printf("%d %d %d %d %d %d %d %d %d %d", d0, d1, d2, d3, d4, d5, d6, d7, d8, d9);
+    
+    return 0;
+}
