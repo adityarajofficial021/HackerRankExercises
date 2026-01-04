0a6692769195b84bafd8b2b034f9d5122b380f4c
Aditya Raj <adityarajofficial021@gmail.com>
2026-01-04 22:20:48 +0530

    Add files via upload

---
+++ Loops_Alternate Positive:Negative Sequence.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+
+    int num;
+    scanf("%d", &num);
+    
+    for(int i = 1; i <= num; ++i){
+        
+    if(i%2 == 1){
+        printf("%d ", i);
+    }
+        else{
+            printf("-%d ",i);
+        }
+    }
+         
+    return 0;
+}
