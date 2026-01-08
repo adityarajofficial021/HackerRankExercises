0b7c7611e3b49b21ae1dd151e2ed89367ca3610b
Aditya Raj <adityarajofficial021@gmail.com>
2026-01-08 20:00:21 +0530

    Add files via upload

---
+++ Loops_Print All Factors of a Number.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    
+    int num;
+    scanf("%d", &num);
+    
+    for(int i = 1; i<=num; ++i){
+        
+        if(num%i);
+        else
+            printf("%d ",i);
+    }
+    return 0;
+}
