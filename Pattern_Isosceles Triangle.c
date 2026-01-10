0512c1ac055468de236237ad4b7a0c1e2eabe357
Aditya Raj <adityarajofficial021@gmail.com>
2026-01-11 00:24:53 +0530

    Add files via upload

---
+++ Pattern_Isosceles Triangle.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    
+    int num;
+    scanf("%d", &num);
+    
+    if(num<=1){
+        printf("Shape Not Possible");
+        return 0;
+        }
+    
+    for(int i = 1; i <= num;++i){
+       
+       for(int j=1; j <= num - i; ++j){
+           printf(" ");
+       }
+        for(int k = 1; k <= i * 2 - 1;++k){
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
