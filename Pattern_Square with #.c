1eefabaaef929e630ff827161a2c6bb6e688813f
Aditya Raj <adityarajofficial021@gmail.com>
2026-01-10 11:43:52 +0530

    Add files via upload

---
+++ Pattern_Square with #.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    
+    int num;
+    scanf("%d", &num);
+    
+    if(num < 3){
+        printf("Shape Not Possible");
+        return 0;
+    }
+    for(int i = 1; i<=num; ++i){
+        
+        for(int j=1; j<=num; ++j){
+            if(i==1 || i==num || j==1 || j==num){
+                printf("*");
+            }
+            else{
+                printf("#");
+            }
+        }
+        printf("\n");
+    }
+    return 0;
+}
